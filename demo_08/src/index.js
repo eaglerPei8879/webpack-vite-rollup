e79685b5d4b03907178c9e473e63ab07e4956505
eaglerPei8879 <3912378879@qq.com>
2026-07-26 08:23:48 +0800

    source-map

---
+++ demo_08/src/index.js
@@ -0,0 +1,19 @@
+// import "./style/index.css";
+// import An from "./assets/image/last.jpg";
+
+document.body.insertAdjacentHTML("beforeend", `<h1>hello webpack</h1>`);
+// document.body.insertAdjacentHTML(
+//   "beforeend",
+//   `<img src="${An}" alt="Last Image">`,
+// );
+
+// const funTest = () => {
+//   console.log("funTest");
+// };
+
+// console.log(funTest());
+document.body.onclick = () => {
+  alert("你点了我耶！");
+};
+
+// console(/(?i:a)b/.test("ab")); // true
