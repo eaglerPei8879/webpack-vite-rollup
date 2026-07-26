8519cad8a02b7954685368a203156d63e1991551
eaglerPei8879 <3912378879@qq.com>
2026-07-26 10:37:51 +0800

    vite-demo01

---
+++ vite_demo_01/src/index.js
@@ -0,0 +1,4 @@
+import m1 from "./m1.js";
+
+document.body.insertAdjacentHTML("beforeend", `<h1>hello vite</h1>`);
+m1.setH2();
---
+++ vite_demo_01/src/m1.js
@@ -0,0 +1,5 @@
+export default {
+  setH2: () => {
+    document.body.insertAdjacentHTML("beforeend", `<h2>hello vite</h2>`);
+  },
+};
